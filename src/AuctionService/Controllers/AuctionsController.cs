18f7e9c3f23f416a92b23bc1dcec3297ae9d8550
LinG-02 <74174742+LinG-02@users.noreply.github.com>
2024-08-12 12:20:56 +1000

    MessageType notFound in OutboxDb

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -57,14 +57,16 @@ public class AuctionsController: ControllerBase
 
         auction.Seller = "test";
 
+        // add message to service bus
         _context.Auctions.Add(auction);
 
-        var result = await _context.SaveChangesAsync() > 0;
-
         var newAuction = _mapper.Map<AuctionDto>(auction);
-        
+
         await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
+
+        var result = await _context.SaveChangesAsync() > 0;
+        
         if (!result) return BadRequest("Could not sage changes to the DB");
 
         return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, newAuction);
